6992ab9dc2ee9231ee8412367e8017c151c63dcd
Ayca Badem <aycabadem12@gmail.com>
2023-09-12 14:06:46 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import SignIn from "./components/Signin";
+import SignUp from "./components/SignUp";
 
 function App() {
+  const [showSignIn, setShowSignIn] = useState(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showSignIn && <SignIn setShowSignIn={setShowSignIn} />}
+      {!showSignIn && <SignUp setShowSignIn={setShowSignIn} />}
     </div>
   );
 }
